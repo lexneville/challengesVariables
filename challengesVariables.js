acab5f7fc9a9312c1d1435a460154b190e1ff7b0
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-20 09:28:08 +0000

    reupload

---
+++ challengesVariables.js
@@ -0,0 +1,23 @@
+// Challenges 1  
+
+let space1 = "x";
+let space2 = "0";
+let space3 = " ";
+let space4 = "x";
+let space5 = "x";
+let space6 = " ";
+let space7 = "0";
+let space8 = " ";
+let space9 = " ";
+
+console.log("      |       |      ");
+console.log(`  ${space1}   |   ${space2}   |   ${space3}  `);
+console.log("      |       |      ");
+console.log("- - - - - - - - - - -");
+console.log("      |       |      ");
+console.log(`  ${space4}   |   ${space5}   |   ${space6}  `);
+console.log("      |       |      ");
+console.log("- - - - - - - - - - -");
+console.log("      |       |      ");
+console.log(`  ${space7}   |   ${space8}   |   ${space9}  `);
+console.log("      |       |      ");
